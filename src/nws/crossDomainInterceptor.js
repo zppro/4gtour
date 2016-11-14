604bdf61144a45b0486db5e9d8e836ff974f06e9
zppro <zpprozppro@gmail.com>
2016-11-14 09:52:07 +0800

    0.2.12a

--- src/nws/crossDomainInterceptor.js
+++ src/nws/crossDomainInterceptor.js
@@ -4,16 +4,21 @@
 
 var cdaOrigins = require('../pre-defined/cda-origins.json');
 module.exports = function (app) {
+    var re = /https?:\/\/192\.168\.([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.([0-9]|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]):?[0-9]*/gi;//内网地址
     return function *(next) {
         console.log('crossDomain');
         var self = this;
-        var origin = this.request.headers['origin'];
-        if (app._.contains(cdaOrigins, origin)) {
+        var origin = (this.request.headers['origin'] || '').toLowerCase();
+        var patched = app._.contains(cdaOrigins, origin) || origin.match(re);
+        if (patched) {
             this.set('Access-Control-Allow-Origin', origin);
             this.set('Access-Control-Allow-Credentials', 'true');
             this.set('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Authorization,Accept,X-Requested-With,Origin,Access-Control-Allow-Origin,X-Custom-TS');
             this.set('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
-        } 
+        }
+        else{
+            console.log('not allowed')
+        }
 
         yield next;
     };
