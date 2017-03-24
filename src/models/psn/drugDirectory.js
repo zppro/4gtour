fce5c3f28813e283a1cb35030ca053c3a38ae46e
yrm <yrm@outlook.com>
2017-03-24 09:43:18 +0800

    change number to boolean

--- src/models/psn/drugDirectory.js
+++ src/models/psn/drugDirectory.js
@@ -16,14 +16,14 @@ module.exports = function(ctx,name) {
             check_in_time: {type: Date, default: Date.now},
             operated_on: {type: Date, default: Date.now},
             status: {type: Number, min: 0, max: 1, default: 1},
-            drug_no:{type: String},// 药品编码
+            drug_no:{type: String，required: true},// 药品编码
             full_name:{type: String},
             short_name:{type: String},
             alias:{type: String},
             english_name:{type: String},
             indications_function:{type: String},//药品功能主治（适用症）
-            otc_flag:{type: Number, min: 0, max: 1},
-            health_care_flag:{type: Number, min: 0, max: 1},
+            otc_flag:{type: Boolean},
+            health_care_flag:{type: Boolean},
             usage:{type: String},
             price:{type: String},
             specification:{type: String},//药品规格
